68e759d16e96d53bbf134a4a52d2a6da03a7d98b
smeshen-2 <kristiyanmitkov25@gmail.com>
2023-11-14 00:00:16 +0200

    added picker to choose what kinds of words you want, made downloading optional on start

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,26 +1,27 @@
 ﻿using ClosedXML.Excel;
 using System.Net;
-using System.Linq;
 
 namespace PhiTranslate;
 
 public partial class App : Microsoft.Maui.Controls.Application
 {
-    public static string appDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    static string appDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
     static string filePath = Path.Join(appDataDirectory, "Pocerpish.xlsx");
     static string temporaryPath = Path.Join(appDataDirectory, "Temporary.xlsx");
-    public static bool fileExists;
+    public static bool fileExists = File.Exists(filePath);
+    static string link = "https://onedrive.live.com/download.aspx?resid=AF050EA4E751ECB!873&authkey=!AA0idZgO-e28Z_Y&ithint=file%2cxlsx&e=FPhvcp";
 
-    public static List<string> pocerpish = new List<string>();
-    public static List<string> english = new List<string>();
+    public static List<string> pocerpishN = new List<string>();
+    public static List<string> pocerpishV = new List<string>();
+    public static List<string> pocerpishA = new List<string>();
+    public static List<string> pocerpishO = new List<string>();
+    public static List<string> englishN = new List<string>();
+    public static List<string> englishV = new List<string>();
+    public static List<string> englishA = new List<string>();
+    public static List<string> englishO = new List<string>();
 
-    public App()
+    public static void Download()
     {
-        InitializeComponent();
-        MainPage = new AppShell();
-
-        string link = "https://onedrive.live.com/download.aspx?resid=AF050EA4E751ECB!873&authkey=!AA0idZgO-e28Z_Y&ithint=file%2cxlsx&e=FPhvcp";
-
         try
         {
             using (var client = new WebClient())
@@ -32,30 +33,59 @@ public partial class App : Microsoft.Maui.Controls.Application
         catch { }
         if (File.Exists(temporaryPath)) File.Delete(temporaryPath);
         fileExists = File.Exists(filePath);
-        if (!fileExists) return;
-
+    }
+    public static void Define()
+    {
         using (var workBook = new XLWorkbook(filePath))
         {
             var sheet = workBook.Worksheet("peresdous");
-            english.AddRange(sheet.Column(3).CellsUsed().Select(c => c.GetText()).Skip(1));
-            english.AddRange(sheet.Column(5).CellsUsed().Select(c => c.GetText()));
-            english.AddRange(sheet.Column(7).CellsUsed().Select(c => c.GetText()));
-            english.AddRange(sheet.Column(9).CellsUsed().Select(c => c.GetText()).Skip(1));
-            english.AddRange(sheet.Column(11).CellsUsed().Select(c => c.GetText()));
-            english.AddRange(sheet.Column(13).CellsUsed().Select(c => c.GetText()).Skip(1));
-            english.AddRange(sheet.Column(15).CellsUsed().Select(c => c.GetText()));
-            for (int i = 0; i < english.Count; i++)
-            {
-                english[i] = english[i].Replace(" ·", "").Replace(" +", "").Replace(" ~", "").Replace(" *", "");
-            }
 
-            pocerpish.AddRange(sheet.Column(4).CellsUsed().Select(c => c.GetText()));
-            pocerpish.AddRange(sheet.Column(6).CellsUsed().Select(c => c.GetText()));
-            pocerpish.AddRange(sheet.Column(8).CellsUsed().Select(c => c.GetText()));
-            pocerpish.AddRange(sheet.Column(10).CellsUsed().Select(c => c.GetText()));
-            pocerpish.AddRange(sheet.Column(12).CellsUsed().Select(c => c.GetText()));
-            pocerpish.AddRange(sheet.Column(14).CellsUsed().Select(c => c.GetText()));
-            pocerpish.AddRange(sheet.Column(16).CellsUsed().Select(c => c.GetText()));
+            englishN.AddRange(sheet.Column(3).CellsUsed().Select(c => c.GetText()).Skip(1));
+            englishN.AddRange(sheet.Column(5).CellsUsed().Select(c => c.GetText()));
+            englishN.AddRange(sheet.Column(7).CellsUsed().Select(c => c.GetText()));
+
+            englishV.AddRange(sheet.Column(9).CellsUsed().Select(c => c.GetText()).Skip(1));
+            englishV.AddRange(sheet.Column(11).CellsUsed().Select(c => c.GetText()));
+
+            englishA.AddRange(sheet.Column(13).CellsUsed().Select(c => c.GetText()).Skip(1));
+            englishA.AddRange(sheet.Column(15).CellsUsed().Select(c => c.GetText()));
+
+            englishO.AddRange(sheet.Column(18).CellsUsed().Where(c => !sheet.Cell(c.Address.RowNumber, c.Address.ColumnNumber + 1).IsEmpty()).Select(c => c.GetText()));
+
+            pocerpishN.AddRange(sheet.Column(4).CellsUsed().Select(c => c.GetText()));
+            pocerpishN.AddRange(sheet.Column(6).CellsUsed().Select(c => c.GetText()));
+            pocerpishN.AddRange(sheet.Column(8).CellsUsed().Select(c => c.GetText()));
+
+            pocerpishV.AddRange(sheet.Column(10).CellsUsed().Select(c => c.GetText()));
+            pocerpishV.AddRange(sheet.Column(12).CellsUsed().Select(c => c.GetText()));
+
+            pocerpishA.AddRange(sheet.Column(14).CellsUsed().Select(c => c.GetText()));
+            pocerpishA.AddRange(sheet.Column(16).CellsUsed().Select(c => c.GetText()));
+
+            pocerpishO.AddRange(sheet.Column(19).CellsUsed().Select(c => c.GetText()));
+
+            for (int i = 0; i < englishN.Count; i++)
+                englishN[i] = englishN[i].Replace(" ·", "").Replace(" +", "").Replace(" ~", "").Replace(" *", "");
+            for (int i = 0; i < englishV.Count; i++)
+                englishV[i] = englishV[i].Replace(" ·", "").Replace(" +", "").Replace(" ~", "").Replace(" *", "");
+            for (int i = 0; i < englishA.Count; i++)
+                englishA[i] = englishA[i].Replace(" ·", "").Replace(" +", "").Replace(" ~", "").Replace(" *", "");
+            for (int i = 0; i < englishO.Count; i++)
+                englishO[i] = englishO[i].Replace(" ·", "").Replace(" +", "").Replace(" ~", "").Replace(" *", "");
         }
+    }
+
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+
+        if (!fileExists)
+        {
+            Download();
+            if (!fileExists) return;
+        }
+        fileExists = true;
+        Define();
     }
 }
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,32 +14,78 @@ public partial class MainPage : ContentPage
             return;
         }
         Button senderB = (Button)sender;
-        string word = (Entry.Text ?? "").ToLower();
-        if (senderB.Text == "To Pocerpish") ToPoc(word);
-        if (senderB.Text == "To English") ToEn(word);
+        string word = (Entry.Text ?? "").ToLower().Trim();
+        try
+        {
+            if (senderB.Text == "To Pocerpish") ToPoc(word);
+            if (senderB.Text == "To English") ToEn(word);
+        }
+        catch { Output.Text = "Unknown word"; }
     }
     private void ToPoc(string word)
     {
         word = word.Replace("remember", "rember 😁").Replace("forget", "forgor 💀");
-        try
+        int item = Picker.SelectedIndex;
+        switch (item)
         {
-            Output.Text = App.pocerpish[App.english.FindIndex(w => w == word)];
-        }
-        catch
-        {
-            Output.Text = "Unknown word";
+            case 1:
+                Output.Text = App.pocerpishN[App.englishN.IndexOf(word)];
+                return;
+            case 2:
+                Output.Text = App.pocerpishV[App.englishV.IndexOf(word)];
+                return;
+            case 3:
+                Output.Text = App.pocerpishA[App.englishA.IndexOf(word)];
+                return;
+            case 4:
+                Output.Text = App.pocerpishO[App.englishO.IndexOf(word)];
+                return;
+            case 0:
+                if (App.englishN.Contains(word)) { Output.Text = App.pocerpishN[App.englishN.IndexOf(word)]; return; }
+                if (App.englishV.Contains(word)) { Output.Text = App.pocerpishV[App.englishV.IndexOf(word)]; return; }
+                if (App.englishA.Contains(word)) { Output.Text = App.pocerpishA[App.englishA.IndexOf(word)]; return; }
+                if (App.englishO.Contains(word)) { Output.Text = App.pocerpishO[App.englishO.IndexOf(word)]; return; }
+                throw new Exception();
         }
     }
     private void ToEn(string word)
     {
-        try
+        int item = Picker.SelectedIndex;
+        switch (item)
         {
-            Output.Text = App.english[App.pocerpish.FindIndex(w => w == word)];
+            case 1:
+                Output.Text = App.englishN[App.pocerpishN.IndexOf(word)];
+                return;
+            case 2:
+                Output.Text = App.englishV[App.pocerpishV.IndexOf(word)];
+                return;
+            case 3:
+                Output.Text = App.englishA[App.pocerpishA.IndexOf(word)];
+                return;
+            case 4:
+                Output.Text = App.englishO[App.pocerpishO.IndexOf(word)];
+                return;
+            case 0:
+                if (App.pocerpishN.Contains(word)) { Output.Text = App.englishN[App.pocerpishN.IndexOf(word)]; return; }
+                if (App.pocerpishV.Contains(word)) { Output.Text = App.englishV[App.pocerpishV.IndexOf(word)]; return; }
+                if (App.pocerpishA.Contains(word)) { Output.Text = App.englishA[App.pocerpishA.IndexOf(word)]; return; }
+                if (App.pocerpishO.Contains(word)) { Output.Text = App.englishO[App.pocerpishO.IndexOf(word)]; return; }
+                throw new Exception();
         }
-        catch
-        {
-            Output.Text = "Unknown word";
-        }
+    }
+    private void Refresh_Clicked(object sender, EventArgs e)
+    {
+        App.Download();
+        if (!App.fileExists) { Output.Text = "No internet connection"; return; }
+        App.pocerpishN.Clear();
+        App.pocerpishV.Clear();
+        App.pocerpishA.Clear();
+        App.pocerpishO.Clear();
+        App.englishN.Clear();
+        App.englishV.Clear();
+        App.englishA.Clear();
+        App.englishO.Clear();
+        App.Define();
     }
 }
 
